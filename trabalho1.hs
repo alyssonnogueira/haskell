c9552e9bf79f72c305ea6e332073f736232f2abb
Alysson Nogueira <anrodrigues@inf.ufpel.edu.br>
2016-07-08 17:06:34 -0300

    while funcionando Atrib ainda não.

--- trabalho1.hs
+++ trabalho1.hs
@@ -29,7 +29,6 @@ data CExp =    While BExp CExp
 meuEstado :: Estado
 meuEstado = [("x",3), ("y",5), ("z",0)]
 
-
 abigStep :: (AExp,Estado) -> (Int,Estado)
 abigStep (Var x,s) = (procuraVar s x,s)
 abigStep (Num n,s) = (n,s)
@@ -42,7 +41,6 @@ abigStep (Sub e1 e2,s)  = let	(n1,s1) = abigStep (e1, s)
 abigStep (Mul e1 e2,s)  = let	(n1, s1) = abigStep (e1, s)
 				(n2, s2) = abigStep(e2, s)
 					in (n1*n2, s)
-
 
 bbigStep :: (BExp,Estado) -> (Bool,Estado)
 bbigStep (TRUE,s)  	= (True,s)
@@ -62,28 +60,26 @@ bbigStep (Or b1 b2,s )  = let 	(bool1,s1) = bbigStep (b1,s)
 			 		True -> (True, s)
 			 		False -> let (bool2,s2) = bbigStep (b2,s) 
 							in (bool2, s)
-					
+
 cbigStep :: (CExp,Estado) -> (CExp,Estado)
-cbigStep (Skip,s)      	= (Skip,s)
-cbigStep (While b c, s) = let (b1,s1) = bbigStep (b,s)
-				in case b1 of
-					True -> let (_,s2) = cbigStep (c,s) 
-						(_,s3) = cbigStep (While b c,s2) 
-							in (Skip,s3)
-					False -> (Skip,s)
+cbigStep (Skip,s) = (Skip,s)
 cbigStep (If b c1 c2,s) = let (b1, s) = bbigStep (b, s) 
                 in case b1 of 
 					True -> let (calc1, s2) = cbigStep (c2, s)
 							in (Skip, s2)
 					False -> let (calc2, s2) = cbigStep (c2, s)
-							in (Skip, s2)		
-cbigStep (Atrib (Var x) e,s) = let (Var x1) = abigStep(x, s)
-						(Var e1) = abigStep(e, s)
-						in (Skip, s[x = e1])
---cbigStep (Do CExp While BExp) =
---cbigStep (Repeat CExp Until BExp) =
---cbigStep (Loop AExp CExp) =
---X,y := E1, E2
+							in (Skip, s2)
+cbigStep (While b c, s) = let (b1,s1) = bbigStep (b,s)
+				in case b1 of
+					True -> let 	(_,s2) = cbigStep (c,s)
+							(_,s3) = cbigStep (While b c,s2)
+							in (Skip,s3)
+					False -> (Skip,s)
+abigStep (Atrib (Var x) e,s)  = let		(_,s1) = abigStep (e, s)
+				(_,s2) = abigStep (x, s)
+					in (Skip, s)
+--cbigStep (Seq c1 c2,s)  = 
+--cbigStep (Atrib (Var x) e,s) = 
 
 exemplo :: AExp
 exemplo = Som (Num 3) (Som (Var "x") (Var "y"))
@@ -98,15 +94,3 @@ teste1 :: BExp
 teste1 = (Ig (Som (Num 3) (Num 3))  (Mul (Num 2) (Num 3)))
 teste2 :: BExp
 teste2 = (And (And (TRUE) (FALSE))  (Or (TRUE) (FALSE)))
-
-
-testec1 :: CExp
-testec1 = (Seq (Seq (Atrib (Var "z") (Var "x")) (Atrib (Var "x") (Var "y"))) 
-		(Atrib (Var "y") (Var "z")))
-testec2 :: CExp
-testec2 = (If (And (TRUE) (FALSE)) (Som (Num 3) (Num 3)) (Mul (Num 2) (Num 3)))
-fatorial :: CExp
-fatorial = (Seq (Atrib (Var "y") (Num 1))
-                (While (Not (Ig (Var "x") (Num 1)))
-                       (Seq (Atrib (Var "y") (Mul (Var "y") (Var "x")))
-                            (Atrib (Var "x") (Sub (Var "x") (Num 1))))))
