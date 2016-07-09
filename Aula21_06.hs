faca6b795b888d827d01a690b07fac3ac2887ae8
Alysson <anrodrigues@inf.ufpel.edu.br>
2016-07-09 17:41:42 -0300

    Removendo arquivos duplicados

--- Aula21_06.hs
+++
@@ -1,6 +0,0 @@
-Implementação Small Step
-
---Arquivos necessários: interpretador Small Step + biblioteca estado 
-
-Objetivo: Completar a semântica ℇ , adicionar tratamento de exceções
-(throw + try catch) + Exemplos de uso
